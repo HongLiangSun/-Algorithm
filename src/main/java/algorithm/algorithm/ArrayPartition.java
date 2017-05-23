21011436d622cfd8d64573ba4c650f02bbccfb06
sunhongliang04 <sunhongliang04@meituan.com>
2017-05-23 17:43:23 +0800

    数组划分

---
+++ src/main/java/algorithm/algorithm/ArrayPartition.java
@@ -0,0 +1,25 @@
+package algorithm.algorithm;
+
+import java.util.Arrays;
+
+/**
+ * 数组划分
+ * @author sunhongliang04
+ */
+public class ArrayPartition {
+	public static void main(String[] args) {
+		int [] nums = {1,2,3,4};
+		int sum = arrayPairSum(nums);
+		System.out.println(sum);
+	}
+	
+	 public static int arrayPairSum(int[] nums) {
+		 int result = 0;
+		 Arrays.sort(nums);
+		 for(int i = 0 ; i <= nums.length -1 ; i+=2){
+			 result += nums[i];
+		 }
+		 return result;
+	 }
+}
+
