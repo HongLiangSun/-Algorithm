9a74f62a4ef1e702b99a18420af273fd4fa58768
sunhongliang04 <sunhongliang04@meituan.com>
2017-05-24 11:24:11 +0800

    增加注释

--- src/main/java/com/shell/algorithm/NumberComplement.java
+++ src/main/java/com/shell/algorithm/NumberComplement.java
@@ -9,10 +9,11 @@ package com.shell.algorithm;
 public class NumberComplement {
 	public static void main(String[] args) {
 		int complement = findComplement(5);
-		System.out.println(complement);
+		System.out.println("数字补码未:"+complement);
 	}
 
 	public static int findComplement(int num) {
+		//找出二进制中最高位为1的位置之前有多少个0
 		int temp = 0;
 		for (int i = 0; i < 32; i++) {
 			if (((num >> i) & 1) == 1) {
